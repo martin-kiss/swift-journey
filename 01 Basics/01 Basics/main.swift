119c8d671e9bda59a5dee2b4f213202583a69201
martin-kiss <kissmartin0216@gmail.com>
2026-09-22 21:12:04 +0200

    .

--- 01 Basics/01 Basics/main.swift
+++ 01 Basics/01 Basics/main.swift
@@ -1,11 +1,24 @@
-//
-//  main.swift
-//  01 Basics
-//
-//  Created by Martin Kiss on 2026. 09. 14..
-//
-
 import Foundation
 
-print("Szia, Világ!")
+//testing out basic things
 
+var age: Int = 19
+let name: String = "Martin"
+
+print("Hello, my name is \(name) and I am \(age) years old.")
+
+//
+
+var num1 = 2; var num2 = 3
+
+func add(_ num1: Int,_ num2: Int) -> Int{
+    return num1 + num2
+}
+
+print("I created a function which sums two numbers: \(add(num1,num2))")
+
+//
+
+if num1 < num2{
+    print("\(num1) is smaller than \(num2)")
+}
